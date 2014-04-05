7d53841b1740162303095348a615af8423e846aa
Straw1239@gmail.com <Rajan@Phantom.PK5001Z>
2014-04-04 19:57:54 -0700

    Test

--- First/src/gui/Fullscreen.java
+++ First/src/gui/Fullscreen.java
@@ -8,17 +8,14 @@ public class Fullscreen
 {
 
 	public static JFrame frame = new JFrame();
-	
+	public static Hangman game = new Hangman();
 	public static void main(String[] args) 
 	{
 		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setUndecorated(true);
-		frame.setBackground(Color.white);
-		Painter painter = new Painter();
-		painter.setSize(frame.getSize());
-		frame.getContentPane().add(painter);
-		frame.setVisible(true);
+		frame.setBackground(Color.cyan);
+		
 	}
 
 }
