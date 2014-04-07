5f45da1692c519aa65b6ed1af15837d18420e0f1
Straw <Straw1239@gmail.com>
2014-04-07 08:55:47 -0700

    Added Multithreading package

---
+++ First/src/theads/ThreadMain.java
@@ -0,0 +1,30 @@
+package theads;
+
+public class ThreadMain {
+
+	public static void main(String[] args) {
+		Thread t = new Thread(new Run());
+		t.run();
+		
+		
+
+	}
+	private static class Run implements Runnable
+	{
+
+		@Override
+		public void run() 
+		{
+			
+		}
+		
+	}
+	
+	private static class RunThread extends Thread
+	{
+		public void run()
+		{
+			
+		}
+	}
+}
