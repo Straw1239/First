490dd573418d0da044cb1d9b4d433669a5eaccfb
Straw <Straw1239@gmail.com>
2014-04-07 09:59:25 -0700

    Started bitwise state class

---
+++ First/src/numbers/BitState.java
@@ -0,0 +1,22 @@
+package numbers;
+
+import java.util.Random;
+
+public class BitState 
+{
+	private static Random rand = new Random();
+	
+	private long state;
+	
+	public byte getSquare(byte row, byte column)
+	{
+		return Byte.MIN_VALUE;//Implement
+	}
+	
+	private void setSquare(byte arg, byte row, byte column)
+	{
+		
+	}
+	
+	
+}
