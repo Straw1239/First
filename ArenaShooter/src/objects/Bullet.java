e6c7b387bba1020ac5c56168ab56515e6069a33a
Straw1239@gmail.com <Straw1239@gmail.com>
2014-11-27 19:16:00 +0000

    Fixed Conflict

--- ArenaShooter/src/objects/Bullet.java
+++ ArenaShooter/src/objects/Bullet.java
@@ -5,6 +5,7 @@ package objects;
 import static fxcore.MainGame.rand;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import objects.Bullet.Builder;
 import objects.entities.Entity;
 import utils.Utils;
 import utils.Vector;
@@ -266,6 +267,11 @@ public class Bullet extends MovingObject implements BulletDataHolder
 		}
 		
 		
+	}
+
+	public static Builder builder()
+	{
+		return new Builder();
 	}
 	
 	
